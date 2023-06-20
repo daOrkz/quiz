daa980e4ecc2a71f7315ccab049bbac27803e171
daOrkz <fillippok87@gmail.com>
2023-06-20 22:26:37 +0700

    Debugger Bar and first layouts

---
+++ resources/views/content/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.content')
+
+@section('content')
+
+<h2>Hello page and Statistic</h2>
+
+@endsection
---
+++ resources/views/layouts/content.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+
+  @include('includes.content.sidebar')
+
+  <h4>layouts.content</h4>
+@endsection
