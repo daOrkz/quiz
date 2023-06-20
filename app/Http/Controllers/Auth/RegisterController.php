1a92c20f988414d258b928c193ecd17ce7fa6697
daOrkz <fillippok87@gmail.com>
2023-06-20 17:33:41 +0700

    create table userName_table on registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,9 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
 class RegisterController extends Controller
 {
     /*
@@ -52,7 +55,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:3', 'confirmed'],
         ]);
     }
 
@@ -64,10 +67,20 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+                
+        $userTableName = $data['name'] . '_table';
+
+        Schema::create($userTableName, function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->string('place');
+        });
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        
     }
 }
