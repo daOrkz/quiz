a5eebead0303e29fe7f48826a5792ec49d3da924
daOrkz <fillippok87@gmail.com>
2023-06-21 22:33:43 +0700

    text for sidebar

--- resources/views/includes/content/sidebar.blade.php
+++ resources/views/includes/content/sidebar.blade.php
@@ -1,15 +1,15 @@
 <ul class="nav flex-column" style="background-color: #e3f2fd; height: 100vh;">
   <li class="nav-item">
-    <a class="nav-link active" href="#">Active</a>
+    <a class="nav-link active" href="#">Пройти тесты</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
+    <a class="nav-link" href="#">Добавить</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
+    <a class="nav-link" href="#">Редактировать</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link">Disabled</a>
+    <a class="nav-link" href="#">Disabled</a>
   </li>
 </ul>
 
