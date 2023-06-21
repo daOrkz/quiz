99bca8e58ad61636d05fa1fa5c0f534413a421b3
daOrkz <fillippok87@gmail.com>
2023-06-21 18:38:48 +0700

    style for sidebar

--- resources/views/includes/content/sidebar.blade.php
+++ resources/views/includes/content/sidebar.blade.php
@@ -1,22 +1,16 @@
-
-
-
-
-      <ul class="nav flex-column">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Active</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link">Disabled</a>
-        </li>
-
-   
-
+<ul class="nav flex-column" style="background-color: #e3f2fd; height: 100vh;">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link">Disabled</a>
+  </li>
+</ul>
 
 
--- resources/views/layouts/content.blade.php
+++ resources/views/layouts/content.blade.php
@@ -11,6 +11,6 @@
         <div class="col-10">
           @yield('quiz-content')
         </div>
-        
+
       </div>
 @endsection
