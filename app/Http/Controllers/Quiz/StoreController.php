631c907967cf4d2cc9b3ad0b0e18538b52140fa9
daOrkz <fillippok87@gmail.com>
2023-06-26 19:51:51 +0700

    Exception StoreController

--- app/Http/Controllers/Quiz/StoreController.php
+++ app/Http/Controllers/Quiz/StoreController.php
@@ -5,34 +5,48 @@ namespace App\Http\Controllers\Quiz;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Quiz\StoreRequest;
 
+use Illuminate\Support\Facades\DB;
+
+use App\Exceptions\DataBaseException;
+
 use App\Models\CorrectAnswer;
 use App\Models\IncorrectAnswer;
 use App\Models\Questions;
 use App\Models\User;
+use Exception;
 
 class StoreController extends Controller {
   
   public function __invoke(StoreRequest $request)
   {
     $data = $request->validated();
-    $userId = auth()->user()->id;
+   
+    try{
+      DB::transaction(function() use ($data) { 
 
-    $question = [
-      'question' => $data['question'],
-    ];
+        $userId = auth()->user()->id;
+
+        $question = [
+          'question' => $data['question'],
+        ];
+            
+        $correct_answer = [
+          'answer' => $data['correct_answer'],
+        ];
+
+        $questionDB = User::find($userId)->question()->firstOrCreate($question);
+
+        $questionDB->correct()->firstOrCreate($correct_answer);
+
         
-    $correct_answer = [
-      'answer' => $data['correct_answer'],
-    ];
-
-    $questionDB = User::find($userId)->question()->firstOrCreate($question);
-
-    $questionDB->correct()->firstOrCreate($correct_answer);
-
-    foreach($data['incorrect_answer'] as $incorrect_answer){
-      $questionDB->incorrect()->firstOrCreate(['answer' => $incorrect_answer]);
+        foreach($data['incorrect_answer'] as $incorrect_answer){
+          $questionDB->incorrect()->firstOrCreate(['answer' => $incorrect_answer]);
+        }
+          
+      }, 3);
+    } catch (Exception $e) {
+      return redirect(route('error'));
     }
-        
 
     return redirect(route('home'));
   }
