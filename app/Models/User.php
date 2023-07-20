2429f8613d28d6c5114d10fc1acb2039466bd430
daOrkz <fillippok87@gmail.com>
2023-07-20 20:07:25 +0700

    migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,8 @@ class User extends Authenticatable
 
         return $this->hasMany(Questions::class);
     }
+
+    public function userRole(){
+        return $this->hasOne(UserRole::class);
+    }
 }
